17fdfaf4beaa169832dcdaf2be6e6a2f3e617c1e
Craig Fleischman <Craigfl@hotmail.com>
2019-09-14 12:14:31 -0700

    Add files via upload

--- forwarder.py
+++ forwarder.py
@@ -1,42 +1,37 @@
 import paho.mqtt.client as mqtt
 
-print ("Running v0.6")
+print ("running v0.11")
 
 # mqtt specifics
-MQTT_HOST="mosquitto"
-MQTT_PORT=1883
-MQTT_TOPIC="test_topic"
+MQTT_HOST = "mosquitto"
+MQTT_PORT = 1883
+MQTT_TOPIC = "test_topic"
 
 # connection event
 def on_connect(client, data, flags, rc):
     if rc==0:
-        client.subscribe(MQTT_TOPIC, 0)
-        print("connected OK")
+         print("connected OK")
+         client.subscribe(MQTT_TOPIC)
     else:
-        print("Bad connection: ", str(rc))
-
-# subscription event
-def on_subscribe(client, userdata, mid, gqos):
-    print("Subscribed: ",  str(mid))
+        print("Bad connection:", str(rc))
 
 # received message event
-def on_message(client, obj, msg):
-    client.connect(MQTT_HOST, MQTT_PORT, 60)
-    print("Msg received")
-    f = open("Face.png", 'w')
+def on_message(client, data, msg):
+    print("Received topic:", str(msg.topic))
+    f = open("face.png", "wb")
     f.write(msg.payload)
     f.close()
 
 # create MQTT client
-client = mqtt.Client(None, clean_session=True)
+client = mqtt.Client()
 
 # assign event callbacks
-client.on_message = on_message
 client.on_connect = on_connect
-client.on_subscribe = on_subscribe
+client.on_message = on_message
 
 # client connection
 client.connect(MQTT_HOST, MQTT_PORT, 60)
-client.subscribe(MQTT_TOPIC, 0)
 
+# start loop
 client.loop_forever()
+
